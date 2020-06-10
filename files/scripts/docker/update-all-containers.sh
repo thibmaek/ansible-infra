2f17ab9d7cdfd4e83c7019588a117291e9716a45
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2020-06-10 17:41:27 +0200

    (ansible): Add script to update docker containers and update docker playbook

---
+++ files/scripts/docker/update-all-containers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+function print_line_break() {
+  # shellcheck disable=SC2183
+  printf '%20s\n' | tr ' ' -
+}
+
+readarray -d '' composeConfigs < <(find . -type f -name "docker-compose.y*" -print0)
+
+for cfg in "${composeConfigs[@]}"; do
+  echo ""
+  echo "[Updating docker-compose containers for config: $cfg]"
+  print_line_break
+  docker-compose -f "$cfg" pull
+  docker-compose -f "$cfg" up -d
+  docker image prune -a --force
+done
+
+# docker image prune
