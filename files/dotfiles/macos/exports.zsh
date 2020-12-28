ea412103229a4b089fa14ca2b2f7efe8cf642728
thibmaek <thibault.maekelbergh@icloud.com>
2020-12-28 18:23:14 +0100

    🔥 Prefer zsh role instead of dotfiles

--- files/dotfiles/macos/exports.zsh
+++
@@ -1,7 +0,0 @@
-# Use brew command-not-found
-if brew command command-not-found-init > /dev/null; then
-  eval "$(brew command-not-found-init)"
-fi
-
-# Use brew sbin to path
-export PATH="/usr/local/sbin:$PATH"
--- files/dotfiles/macos/functions.zsh
+++
@@ -1,3 +0,0 @@
-function cdf() {
-  cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')" || return;
-}
