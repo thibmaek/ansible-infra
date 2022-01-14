2b107be7b8c8ab640046a5fc5c370995617738f9
thibmaek <thibault.maekelbergh@icloud.com>
2022-01-14 18:36:43 +0100

    ✨ (ansible): Set Python3 globally + ask for vault pass

---
+++ keychain_pass.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Keychain query fields.
+# LABEL is the value you put for "Keychain Item Name" in Keychain.app.
+LABEL="ansible-vault-password"
+ACCOUNT_NAME="$(whoami)"
+
+function set_password() {
+  /usr/bin/security add-generic-password -a "$ACCOUNT_NAME" -s "$LABEL" -w
+}
+
+function get_password() {
+  /usr/bin/security find-generic-password -w -a "$ACCOUNT_NAME" -l "$LABEL"
+}
+
+"${@:-get_password}"
